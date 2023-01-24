7df69de5e42a784b5b874e7bcb3244a0e53c4f6b
Atharva Bhawsar <atharvabhawsar18@gmail.com>
2023-01-24 19:38:12 +0530

    Logo and Avatar added - Raw

---
+++ src/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+    return <div className='nav'>
+        <div className="nav_content">
+            <img
+            className='nav_logo'
+            src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png"
+            alt="" />
+        
+        <img
+            className='nav_avatar'
+            src='https://e7.pngegg.com/pngimages/76/633/png-clipart-mr-peanutbutter-desktop-animation-bojack-horseman-season-4-netflix-animation-horse-mammal-thumbnail.png'
+            alt="" />
+        </div>
+        
+      <h2>This is a Nav</h2>
+  </div>
+  
+}
+
+export default Nav

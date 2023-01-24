e1ca7e982af81ae7990428e0c03363f9aedad587
Atharva Bhawsar <atharvabhawsar18@gmail.com>
2023-01-24 14:44:58 +0530

    initialized hollow react app with redux template and firebase key

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCbIWRhRl2Xa98-dBjPQlmhtBgkWXh_9N0",
+  authDomain: "netflix-clone-8a5c4.firebaseapp.com",
+  projectId: "netflix-clone-8a5c4",
+  storageBucket: "netflix-clone-8a5c4.appspot.com",
+  messagingSenderId: "377683055983",
+  appId: "1:377683055983:web:0b12ee2db21a0d77f3049a"
+};

b816d885e2fdf8ca38850f2100b80f563f457c1f
Atharva Bhawsar <atharvabhawsar18@gmail.com>
2023-01-27 21:28:36 +0530

    Image backdrop added

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -30,7 +30,7 @@ function Banner() {
             className='banner'
             style={{
                 backgroundSize: "cover",
-                backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/c/cd/Black_flag.svg")`,
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
                 backgroundPosition: "center center",
             }}>
             
